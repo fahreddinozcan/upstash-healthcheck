0c761be45834279cfdf0cf47d5a83b665812b719
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-09-12 09:46:42 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -212,7 +212,7 @@ export default function Home() {
       <div className="w-full flex-col flex items-center justify-center gap-10 mt-20">
         <Card className="w-min p-4 mt-5">
           <CardHeader>
-            <CardTitle>Health Check, {REDISurl? "YES": "NO"}, {token? "YES": "NO"}</CardTitle>
+            <CardTitle>testHealth Check, {REDISurl? "YES": "NO"}, {token? "YES": "NO"}</CardTitle>
             <Description url={url} cron={schedule} />
           </CardHeader>
           <CardContent>
