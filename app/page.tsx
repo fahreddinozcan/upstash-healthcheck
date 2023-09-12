cd295b8e6ddd8025338153ab02b50a092f8764ec
Fahreddin Özcan <ozcanfahrettinn@gmail.com>
2023-09-12 10:09:26 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,9 @@ import { Chart } from "./components/Chart";
 import { EditForm } from "./components/EditForm";
 import { Spinner } from "./components/Spinner";
 
-const UPSTASHurl = process.env.UPSTASH_REDIS_REST_URL;
-const UPSTASHtoken = process.env.UPSTASH_REDIS_REST_TOKEN
-const REDISurl = process.env.REST_URL
+const UPSTASHurl = process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL;
+const UPSTASHtoken = process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN
+const REDISurl = process.env.NEXT_PUBLIC_REST_URL
 const REDIStoken = process.env.REST_TOKEN
 const tokenVALUE = process.env.NEXT_PUBLIC_TOKEN_VALUE
 const redis = new Redis({
